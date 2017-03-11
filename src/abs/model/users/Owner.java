b288d1b07b25562f77aca12ca4e609632d5c15ac
Sotoam Bak <s3542733@student.rmit.edu.au>
2017-03-11 19:21:45 +1100

    Corrected Owner Class.

--- src/abs/model/users/Owner.java
+++ src/abs/model/users/Owner.java
@@ -3,15 +3,22 @@ package abs.model.users;
 import java.io.*;
 
 public class Owner extends Client{
-	
 
 	private String businessName;
-	private File ownerInfo = new File("ownerInfo.txt");
 	
 	public Owner(String name, String userName, String userPassword, String userAddress, 
 			String userPhoneNumber, String userRole, String businessName)
 	{
 		super(name, userName, userPassword, userAddress, userPhoneNumber, userRole);
 		this.businessName = businessName;
-	}	
+	}
+	
+	public String getBusinessName() {
+		return businessName;
+	}
+
+	public void setBusinessName(String businessName) {
+		this.businessName = businessName;
+	}
+	
 }
