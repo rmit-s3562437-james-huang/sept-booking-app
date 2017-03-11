0cff6196c244783e98c6400101a6e3389d43bd25
Sotoam Bak <s3542733@student.rmit.edu.au>
2017-03-11 19:08:34 +1100

    Added constructor for the owner class.

--- src/abs/model/users/Owner.java
+++ src/abs/model/users/Owner.java
@@ -8,30 +8,10 @@ public class Owner extends Client{
 	private String businessName;
 	private File ownerInfo = new File("ownerInfo.txt");
 	
-	///public Owner()
+	public Owner(String name, String userName, String userPassword, String userAddress, 
+			String userPhoneNumber, String userRole, String businessName)
 	{
-		///Open ownerInfo.txt
-		///Extract businessName, name, userName, userPassword,
-		///userAddress, userPhoneNumber
-		
-		///super(name, userName, userPassword, userAddress, userPhoneNumber, "Owner");
-		///this.businessName = businessName;
-	}
-	
-	///public void addEmployee(String userName, String userPassword, String userAddress, 
-							///String userPhoneNumber, String employeeID)
-	{
-		///Employee newEmployee = new Employee(userName, userPassword, userAddress, 
-							///userPhoneNumber, employeeID);
-		
-		///Write new employee information into employees.txt?
-	}
-	
-	///public void removeEmployee(String EmployeeID)
-	{
-		///Open employees.txt?
-		///Find employee information based of employeeID?
-		///Remove employee information?
-		///Close employees.txt?
-	}
+		super(name, userName, userPassword, userAddress, userPhoneNumber, userRole);
+		this.businessName = businessName;
+	}	
 }
