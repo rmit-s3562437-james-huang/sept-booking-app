ed9088fb6b5b563f974d147d661a044e96fe9612
Sotoam Bak <s3542733@student.rmit.edu.au>
2017-03-11 16:13:48 +1100

    abs.model.users class prototypes. At the moment there is constructor method implemented for Owner as it requires the use of a textfile. I will attempt to implement it in the next update.

--- src/abs/model/users/Employee.java
+++ src/abs/model/users/Employee.java
@@ -1,5 +1,14 @@
 package abs.model.users;
 
 public class Employee extends Client{
-
+	
+	private String userRole = "Employee";
+	private String employeeID;
+	
+	public Employee(String userName, String userPassword, String userAddress, String userPhoneNumber, String EmployeeID)
+	{
+		super(userName, userPassword, userAddress, userPhoneNumber);
+		this.employeeID = employeeID;
+	}
+	
 }
