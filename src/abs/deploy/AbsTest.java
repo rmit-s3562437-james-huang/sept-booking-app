e2c384b97253af8e5bf75206049d3202b5e7a581
rmit-s3562437-james-huang <s3562437@student.rmit.edu.au>
2017-03-13 16:15:42 +1100

    Path fix

--- src/abs/deploy/AbsTest.java
+++ src/abs/deploy/AbsTest.java
@@ -6,7 +6,7 @@ public class AbsTest {
 	
 	public static void main(String[] args)
 	{
-		AbsFileOps file = new AbsFileOps("C:\\Users\\Sotoam Bak\\Documents\\Repositories\\SEPT2017\\", "ownerInfo");
+		AbsFileOps file = new AbsFileOps("/Users/James/git/SEPT2017/", "ownerInfo");
 		file.readFromOwner();
 	}
 	
