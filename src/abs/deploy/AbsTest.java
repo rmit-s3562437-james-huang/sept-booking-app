7ea2ba00ae4265dcd1cd5a622647e1cae501949f
rmit-s3562437-james-huang <s3562437@student.rmit.edu.au>
2017-03-25 15:09:43 +1100

    ...

--- src/abs/deploy/AbsTest.java
+++ src/abs/deploy/AbsTest.java
@@ -1,26 +1,13 @@
 package abs.deploy;
 
-import abs.model.AbsLoginImpl;
 import abs.model.AbsMenuImpl;
-import abs.model.AbsRegisterImpl;
-import abs.model.data.AbstractData;
-import abs.model.data.CustomerData;
-import abs.model.interfaces.Client;
-import abs.model.interfaces.Data;
-import abs.model.users.AbstractClient;
-import abs.model.users.Customer;
-import abs.model.users.Owner;
-import abs.model.AbsClientSystemImpl;
 
 public class AbsTest {
 	
 	public static void main(String[] args)
 	{
-		AbstractData data = new CustomerData();
 		AbsMenuImpl menu = new AbsMenuImpl();
-		
-		menu.initialMenu();
-		
+		menu.initialMenu();	
 	}
 
 }
