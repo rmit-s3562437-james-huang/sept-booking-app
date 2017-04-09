bc49e53d40266e9d78ad59d2efbac1b885427a23
rmit-s3562437-james-huang <s3562437@student.rmit.edu.au>
2017-04-10 04:03:54 +1000

    path change

--- src/abs/view/AbsTest.java
+++ src/abs/view/AbsTest.java
@@ -14,28 +14,19 @@ import abs.model.bookings.Booking;
 import abs.model.users.Customer;
 import abs.model.users.Owner;
 
-/* HOW TO RUN CODE:
+/* 
+ * HOW TO RUN CODE:
  * Place the path of this package in PATHNAME 
  * To run the code; right click AbsTest.java and run as Java Application
- * 
- * MAC OS:
- * Follow EXAMPLE 1 syntax
- * 
- * WINDOWS:
- * Follow EXAMPLE 2 syntax
  */
 
 public class AbsTest {
 	
 	static Logger LOGGER = Logger.getLogger(AbsTest.class.getName());
-		
-	/* SET YOUR PATH HERE */
-	//public final static String PATHNAME = ...
 	
-	/* EXAMPLE 1 */
-	//public final static String PATHNAME = "/Users/James/git/SEPT2017/";
-	
-	/* EXAMPLE 2 */
+	/* DEFUALT STATIC PATH */
+	//public final static String PATHNAME = "../SEPT2017/";
+	public final static String PATHNAME = "/Users/James/git/SEPT2017/";
 	//public final static String PATHNAME = "D:\\Repositories\\SEPT2017\\SEPT2017\\";
 	
 	public final static String CUSTOMERWRITEFILEPATH = PATHNAME + "customerInfo" + ".txt";
