d752bcff372f34c72cb4bdddf49f7669e5372c6e
Sotoam Bak <s3542733@student.rmit.edu.au>
2017-04-26 00:01:32 +1000

    Changed directories...
    Just changed the directory to suit my own desktop.

--- src/abs/view/AbsTest.java
+++ src/abs/view/AbsTest.java
@@ -27,8 +27,8 @@ public class AbsTest {
 	
 	/* DEFUALT STATIC PATH */
 	//public final static String PATHNAME = "../SEPT2017/";
-	public final static String PATHNAME = "/Users/James/git/SEPT2017/";
-	//public final static String PATHNAME = "D:\\Repositories\\SEPT2017\\SEPT2017\\";
+	//public final static String PATHNAME = "/Users/James/git/SEPT2017/";
+	public final static String PATHNAME = "D:\\Repositories\\SEPT2017\\SEPT2017\\";
 	
 	public final static String CUSTOMERWRITEFILEPATH = PATHNAME + "customerInfo" + ".txt";
 	public final static String BOOKINGWRITEFILEPATH = PATHNAME + "bookingTest" + ".txt";
