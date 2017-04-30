5c05e9a08965ccc9a106653a93f7cf7b95a11347
rmit-s3562437-james-huang <s3562437@student.rmit.edu.au>
2017-04-30 23:41:17 +1000

    removed booking done

--- src/abs/view/dialog/customer/RemoveTimeDialog.java
+++ src/abs/view/dialog/customer/RemoveTimeDialog.java
@@ -2,6 +2,9 @@ package abs.view.dialog.customer;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map.Entry;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -10,9 +13,11 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import abs.model.bookings.Booking;
+import abs.view.AbsTest;
 import abs.view.gui.MainCustomerFrame;
 
-public class RemoveTimeDialog extends JDialog {
+public class RemoveTimeDialog extends JDialog implements ActionListener {
 
 	private MainCustomerFrame mainFrame;
 	
@@ -42,6 +47,12 @@ public class RemoveTimeDialog extends JDialog {
 		cbDay = new JComboBox();
 		cbTime = new JComboBox();
 		
+		for (int i = 0; i < BookTimeDialog.DAYS_OF_WEEK.length; i++) {
+			cbDay.addItem(BookTimeDialog.DAYS_OF_WEEK[i]);
+		}
+		
+		cbDay.addActionListener(this);
+		
 		selectDayLabel = new JLabel("Select day: ");
 		selectTimeLabel = new JLabel("Select time: ");
 		
@@ -50,8 +61,10 @@ public class RemoveTimeDialog extends JDialog {
 		removePanel.add(selectTimeLabel);
 		removePanel.add(cbTime);
 		
-		okButton = new JButton("OK");
+		okButton = new JButton("Delete");
+		okButton.addActionListener(this);
 		cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(this);
 		
 		buttonPanel.add(okButton);
 		buttonPanel.add(cancelButton);
@@ -63,6 +76,58 @@ public class RemoveTimeDialog extends JDialog {
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(cancelButton)) {
+			dispose();
+		}
+		
+		cbTime.removeAllItems();
+		for (Booking book : mainFrame.getAbsMaps().getBookingMap().values()) {
+			if (book.getDay().equals(appendDay((String) cbDay.getSelectedItem()))) {
+				if(book.getCustomerUserName().equals(mainFrame.getCustomer().getUserName())) {
+					for(int i = 0; i < book.getTimeSlot().size(); i++) {
+						cbTime.addItem(book.getTimeSlot().get(i));
+					}
+				}
+			}
+		}
+		
+		if (e.getSource().equals(okButton)) {
+			for (Booking book : mainFrame.getAbsMaps().getBookingMap().values()) {
+				for(int i = 0; i < book.getTimeSlot().size(); i++) {
+					if (book.getTimeSlot().get(i).equals(cbTime.getSelectedItem())) {
+						if(book.getDay().equals(appendDay((String) cbDay.getSelectedItem()))) {
+							if (book.getCustomerUserName().equals(mainFrame.getCustomer().getUserName())) {
+								book.getTimeSlot().remove(cbTime.getSelectedItem());
+								mainFrame.getFileOps().compileBookingMapStrings(AbsTest.BOOKINGWRITEFILEPATH, 
+										mainFrame.getAbsMaps().getBookingMap());
+							}
+						}
+					}
+				}
+			}
+			dispose();
+		}
+		
+		java.util.Iterator<Entry<String, Booking>> iter = mainFrame.getAbsMaps().getBookingMap().entrySet().iterator();
+		while (iter.hasNext()) {
+		    Entry<String, Booking> entry = iter.next();
+		    if(mainFrame.getAbsMaps().getBookingMap().get(entry.getKey()).getTimeSlot().isEmpty()){
+		        iter.remove();
+		    }
+		    mainFrame.getFileOps().compileBookingMapStrings(AbsTest.BOOKINGWRITEFILEPATH, 
+					mainFrame.getAbsMaps().getBookingMap());
+		}
+		
+	}
 	
-	
+	/* need to remove after refactor */
+	public String appendDay(String day) {
+		String appendDay;
+		appendDay = day.substring(0, (day.length()-(day.length()-3)));
+		return appendDay;
+	}
 }
