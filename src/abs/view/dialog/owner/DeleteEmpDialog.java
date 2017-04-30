5b1396c5ec1b8d37757898faab296becc8c0ab4d
rmit-s3562437-james-huang <s3562437@student.rmit.edu.au>
2017-04-30 20:47:42 +1000

    owner done

--- src/abs/view/dialog/owner/DeleteEmpDialog.java
+++ src/abs/view/dialog/owner/DeleteEmpDialog.java
@@ -1,11 +1,89 @@
 package abs.view.dialog.owner;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import abs.model.users.Employee;
+import abs.view.AbsTest;
 import abs.view.gui.MainOwnerFrame;
 
-public class DeleteEmpDialog {
+public class DeleteEmpDialog extends JDialog implements ActionListener {
+	
+	private JLabel selectEmployeeLabel;
+	private JComboBox cbEmployee;
+	
+	private JButton okButton;
+	private JButton cancelButton;
+	
+	private JPanel buttonPanel;
+	private JPanel deletePanel;
+	
+	private MainOwnerFrame mainOwnerFrame;
 
 	public DeleteEmpDialog(MainOwnerFrame mainOwnerFrame) {
-		// TODO Auto-generated constructor stub
+		setTitle("Delete Employee");
+		setLayout(new BorderLayout());
+		this.mainOwnerFrame = mainOwnerFrame;
+		
+		deletePanel = new JPanel();
+		deletePanel.setLayout(new GridLayout(0, 2));
+		deletePanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		buttonPanel = new JPanel();
+		
+		cbEmployee = new JComboBox();
+		
+		for(Employee employee : mainOwnerFrame.getAbsMaps().getEmployeeMap().values()) {
+			cbEmployee.addItem(employee.getUserName());
+		}
+		
+		cbEmployee.addActionListener(this);
+		
+		okButton = new JButton("Delete");
+		okButton.addActionListener(this);
+		cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(this);
+		
+		buttonPanel.add(okButton);
+		buttonPanel.add(cancelButton);
+		
+		selectEmployeeLabel = new JLabel("Select employee: ");
+		deletePanel.add(selectEmployeeLabel);
+		deletePanel.add(cbEmployee);
+		
+		add(deletePanel, BorderLayout.CENTER);
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		setSize(260, 120);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(cancelButton)) {
+			dispose();
+		}
+		
+		if (e.getSource().equals(okButton)) {
+			for(Employee employee : mainOwnerFrame.getAbsMaps().getEmployeeMap().values()) {
+				if (employee.getUserName().equals(cbEmployee.getSelectedItem())) {
+					System.out.println(employee.getUserName());
+					mainOwnerFrame.getAbsMaps().getEmployeeMap().remove(employee.getUserName());
+					mainOwnerFrame.getFileOps().compileEmployeeMapStrings(AbsTest.EMPLOYEEWRITEFILEPATH, 
+							mainOwnerFrame.getAbsMaps().getEmployeeMap());
+				}
+			}
+			dispose();
+		}
 	}
 
 }
--- src/abs/view/dialog/owner/ViewEmpDialog.java
+++ src/abs/view/dialog/owner/ViewEmpDialog.java
@@ -1,6 +1,10 @@
 package abs.view.dialog.owner;
 
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import abs.model.users.Employee;
@@ -8,19 +12,38 @@ import abs.view.gui.MainOwnerFrame;
 
 public class ViewEmpDialog extends JDialog {
 
-	private JPanel bookPanel;
+	private JPanel viewPanel;
 	
 	public ViewEmpDialog(MainOwnerFrame mainOwnerFrame) {
 		setTitle("Employee Details");
 		
+		viewPanel = new JPanel();
+		
+		viewPanel.setLayout(new GridLayout(0, 5));
+		
+		viewPanel.setBorder(BorderFactory.createEmptyBorder(2, 40, 2, 10));
+		
+		viewPanel.add(new JLabel("Name"));
+		viewPanel.add(new JLabel("Username"));
+		viewPanel.add(new JLabel("Password"));
+		viewPanel.add(new JLabel("Address"));
+		viewPanel.add(new JLabel("Phone Number"));
+		
+		
 		for (Employee employee : mainOwnerFrame.getAbsMaps().getEmployeeMap().values()) {
 			System.out.println(employee);
+			
+			viewPanel.add(new JLabel(employee.getName()));
+			viewPanel.add(new JLabel(employee.getUserName()));
+			viewPanel.add(new JLabel(employee.getUserPassword()));
+			viewPanel.add(new JLabel(employee.getUserAddress()));
+			viewPanel.add(new JLabel(employee.getUserPhoneNumber()));
 		}
 		
+		add(viewPanel);
 		
-		setSize(800, 300);
+		setSize(700, 200);
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
-
 }
