684deb94a4cff1cb90c55fe0158eaaabf7db8af3
rmit-s3562437-james-huang <s3562437@student.rmit.edu.au>
2017-05-01 02:14:08 +1000

    Damiens bordering

--- src/abs/view/dialog/customer/DisplayBookingDialog.java
+++ src/abs/view/dialog/customer/DisplayBookingDialog.java
@@ -23,24 +23,46 @@ public class DisplayBookingDialog extends JDialog {
 		
 		bookPanel.setLayout(new GridLayout(0, 4));
 		
-		bookPanel.add(new JLabel("Customer"));
-		bookPanel.add(new JLabel("Employee"));
-		bookPanel.add(new JLabel("Day"));
-		bookPanel.add(new JLabel("Times"));
+		
+		JLabel customerLabel = new JLabel("Customer");
+		customerLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		JLabel employeeLabel = new JLabel("Employee");
+		employeeLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		JLabel dayLabel = new JLabel("Day");
+		dayLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		JLabel timesLabel = new JLabel("Times");
+		timesLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		bookPanel.add(customerLabel);
+		bookPanel.add(employeeLabel);
+		bookPanel.add(dayLabel);
+		bookPanel.add(timesLabel);
 		
 		for (Booking book : mainFrame.getAbsMaps().getBookingMap().values()) {
+
 			System.out.println(mainFrame.getCustomer().getUserName());
 			System.out.println(book.getCustomerUserName());
 			if (book.getCustomerUserName().equals(mainFrame.getCustomer().getUserName())) {
 				
-				bookPanel.add(new JLabel(book.getEmployeeUserName()));
-				bookPanel.add(new JLabel(book.getCustomerUserName()));
-				bookPanel.add(new JLabel(book.getDay()));
-				bookPanel.add(new JLabel(book.getTimeSlot().toString()));
+				JLabel empUserLabel = new JLabel(book.getEmployeeUserName());
+				empUserLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				JLabel custUserLabel = new JLabel(book.getCustomerUserName());
+				custUserLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				JLabel getDayLabel = new JLabel(book.getDay());
+				getDayLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				JLabel getTimeLabel = new JLabel(book.getTimeSlot().toString());
+				getTimeLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				
+				bookPanel.add(empUserLabel);
+				bookPanel.add(custUserLabel);
+				bookPanel.add(getDayLabel);
+				bookPanel.add(getTimeLabel);
+				
+				
 			}
 		}
 		
-		bookPanel.setBorder(BorderFactory.createEmptyBorder(2, 50, 2, 10));
+		bookPanel.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
 		
 		add(bookPanel);
 		
